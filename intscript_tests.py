d3564b78c1c9d1340564bdb103ffa501981d85be
Alex Gulliver <randdalf@gmail.com>
2019-12-08 17:46:58 +0000

    Replace all memory tests with execution tests

--- intscript_tests.py
+++ intscript_tests.py
@@ -67,12 +67,6 @@ class IntcodeTests(unittest.TestCase):
                 cls.memstart = i + 1
                 break
 
-    def memory_test(slf, program, expected):
-        """After execution, check that the memory is as expected."""
-        cpu = intcode(slf.intcode, len(program), *program)
-        actual = cpu.memory[slf.memstart:slf.memstart+len(program)]
-        slf.assertEqual(actual, expected)
-
     def execution_test(slf, program, *inputs):
         """Check that a program runs identically on a real intcode computer."""
         actual = intcode(slf.intcode, len(program), *program, *inputs)
@@ -89,34 +83,19 @@ class IntcodeTests(unittest.TestCase):
         slf.execution_test(fibonacci, 10)
 
     def test_d02_example1(slf):
-        slf.memory_test(
-            [1, 0, 0, 0, 99],
-            [2, 0, 0, 0, 99]
-        )
+        slf.execution_test([1, 0, 0, 0, 99])
 
     def test_d02_example2(slf):
-        slf.memory_test(
-            [2, 3, 0, 3, 99],
-            [2, 3, 0, 6, 99]
-        )
+        slf.execution_test([2, 3, 0, 3, 99])
 
     def test_d02_example3(slf):
-        slf.memory_test(
-            [2, 4, 4, 5, 99, 0],
-            [2, 4, 4, 5, 99, 9801]
-        )
+        slf.execution_test([2, 4, 4, 5, 99, 0])
 
     def test_d02_example4(slf):
-        slf.memory_test(
-            [1, 1, 1, 4, 99, 5, 6, 0, 99],
-            [30, 1, 1, 4, 2, 5, 6, 0, 99]
-        )
+        slf.execution_test([1, 1, 1, 4, 99, 5, 6, 0, 99])
 
     def test_d02_example5(slf):
-        slf.memory_test(
-            [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
-            [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
-        )
+        slf.execution_test([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])
 
     def test_d02_puzzle(slf):
         puzzle = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,2,19,6,23,1,23,5,27,1,9,27,31,1,31,10,35,2,35,9,39,1,5,39,43,2,43,9,47,1,5,47,51,2,51,13,55,1,55,10,59,1,59,10,63,2,9,63,67,1,67,5,71,2,13,71,75,1,75,10,79,1,79,6,83,2,13,83,87,1,87,6,91,1,6,91,95,1,10,95,99,2,99,6,103,1,103,5,107,2,6,107,111,1,10,111,115,1,115,5,119,2,6,119,123,1,123,5,127,2,127,6,131,1,131,5,135,1,2,135,139,1,139,13,0,99,2,0,14,0"
